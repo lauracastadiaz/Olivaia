56b0c04968f1c5e837536cdf333158d1d268bd87
Laura Castaño <lauracastadiaz@gmail.com>
2023-12-04 16:53:17 +0100

    subiendo

---
+++ funcion.js
@@ -0,0 +1,8 @@
+ //para el desplegable del nav en pantallas pequeñas 
+
+ $(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+       $('#collapsibleNavbar').toggle();
+    });
+ });
+  
